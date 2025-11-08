4e2f305091e0ae5e46f5211bd0a3390fd3382b48
Ingrid <mirey1806@gmail.com>
2025-11-08 15:04:52 -0600

    Corrige el código y muestra el componente correctamente

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,23 +1,25 @@
-// Import the functions you need from the SDKs you need
+// src/plugins/firebase.js
+
+// Importar funciones necesarias de Firebase
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // 👈 Faltaba esta línea
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuración de Firebase (copiada desde tu consola)
 const firebaseConfig = {
   apiKey: "AIzaSyDAYS_Mwi95qT9XA94NhIxOci7VMtNEDLw",
   authDomain: "proyecto5-f2209.firebaseapp.com",
   projectId: "proyecto5-f2209",
-  storageBucket: "proyecto5-f2209.firebasestorage.app",
+  storageBucket: "proyecto5-f2209.appspot.com", // 👈 corregido aquí
   messagingSenderId: "511625918855",
   appId: "1:511625918855:web:b71df1ca0a43948585bb22",
   measurementId: "G-7KB0L61QDX"
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+
+// Exportar para uso en Vue
 export { app, analytics, db };
